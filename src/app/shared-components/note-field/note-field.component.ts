298a2b5433e30d1b272c200c1222d273d6cee9c3
Ivan <zrosji@gmail.com>
2023-02-23 22:34:38 +0100

    input field buttons + theming

--- src/app/shared-components/note-field/note-field.component.ts
+++ src/app/shared-components/note-field/note-field.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  animation,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { Note } from '../input-bar/input-bar.component';
 
@@ -5,7 +12,56 @@ import { Note } from '../input-bar/input-bar.component';
   selector: 'app-note-field',
   templateUrl: './note-field.component.html',
   styleUrls: ['./note-field.component.scss'],
+  animations: [
+    trigger('icons', [
+      transition(':enter', [
+        style({
+          opacity: '0',
+        }),
+        animate(
+          '200ms ease-in',
+          style({
+            opacity: '1',
+          })
+        ),
+      ]),
+      transition(':leave', [
+        style({
+          opacity: '1',
+        }),
+        animate(
+          '300ms ease-out',
+          style({
+            opacity: '0',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class NoteFieldComponent {
   @Input() note!: Note;
+
+  showButtons = false;
+
+  getTitle() {
+    let clearedText = this.note.title.trim();
+    if (clearedText.length === 0) {
+      return 'No title';
+    }
+    return clearedText;
+  }
+
+  getContent() {
+    let clearedText = this.note.content.trim();
+    if (clearedText.length === 0) {
+      return '';
+    }
+    if (this.note.content.length > 45) {
+      let slicedText = this.note.content.slice(0, 45);
+      let textWithDots = slicedText + '...';
+      return textWithDots;
+    }
+    return clearedText;
+  }
 }
